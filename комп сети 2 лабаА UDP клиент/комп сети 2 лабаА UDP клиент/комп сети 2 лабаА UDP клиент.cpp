6b419151fc12572bfb5246387e2ab5b271efbf27
Diana Saakyan <diana.saakyan003@gmail.com>
2026-01-04 18:04:11 +0300

    Add files via upload

---
+++ комп сети 2 лабаА UDP клиент/комп сети 2 лабаА UDP клиент/комп сети 2 лабаА UDP клиент.cpp
@@ -0,0 +1,88 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <string>
+#include <locale>
+
+using namespace std;
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define SRV_HOST "127.0.0.1"
+#define SRV_PORT 1234
+#define BUF_SIZE 64
+
+struct Student {
+    char name[50];
+    int grades[4];
+} S;
+
+string getBonusText(const string& code) {
+    if (code == "NO_BONUS") return "Без стипендии((";
+    else if (code == "STANDARD_BONUS") return "Стандартная стипендия";
+    else if (code == "HIGH_BONUS") return "Повышенная стипендия (молодец!!)";
+    else return "Неизвестный код";
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    WSADATA wsa;
+    SOCKET sock;
+    sockaddr_in servAddr;
+    int addrSize = sizeof(servAddr);
+
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(SRV_PORT);
+    InetPtonA(AF_INET, SRV_HOST, &servAddr.sin_addr);
+
+    cout << "UDP сервер подключен!" << endl;
+    cout << "Вводите данные или 'exit' для выхода" << endl;
+
+    while (true) {
+        cout << "\n---  Студентик ---" << endl;
+        cout << "Имя (или 'exit'): ";
+        cin.getline(S.name, 50);
+
+        if (string(S.name) == "exit") {
+            sendto(sock, (char*)&S, sizeof(S), 0,
+                (sockaddr*)&servAddr, addrSize);
+            cout << "Выход..." << endl;
+            break;
+        }
+
+        cout << "Введите оценки за экзамены: ";
+        for (int i = 0; i < 4; i++) {
+            cin >> S.grades[i];
+        }
+
+        cin.ignore();
+
+        sendto(sock, (char*)&S, sizeof(S), 0,
+            (sockaddr*)&servAddr, addrSize);
+
+        cout << "Данные отправлены" << endl;
+
+        char bonusCode[50];
+        int received = recvfrom(sock, bonusCode, sizeof(bonusCode), 0,
+            (sockaddr*)&servAddr, &addrSize);
+
+        if (received > 0) {
+            string code(bonusCode);
+            string bonusText = getBonusText(code);
+
+            cout << "\n>>> РЕЗУЛЬТАТ <<<" << endl;
+            cout << "Студент: " << S.name << endl;
+            cout << "Стипендия: " << bonusText << endl;
+            cout << "----------------" << endl;
+        }
+    }
+
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
---
+++ комп сети 2 лабаА UDP сервер/комп сети 2 лабаА UDP сервер/комп сети 2 лабаА UDP сервер.cpp
@@ -0,0 +1,94 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <string>
+#include <locale>
+
+using namespace std;
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define SRV_PORT 1234
+
+
+struct Student {
+    char name[50];
+    int grades[4];
+} S;
+
+string calcScholarship() {
+    bool hasThree = false;
+    bool allFives = true;
+
+    for (int i = 0; i < 4; i++) {
+        if (S.grades[i] == 3) {
+            hasThree = true;
+        }
+        if (S.grades[i] != 5) {
+            allFives = false;
+        }
+    }
+
+    if (hasThree) {
+        return "NO_BONUS";
+    }
+    else if (allFives) {
+        return "HIGH_BONUS";
+    }
+    else {
+        return "STANDARD_BONUS";
+    }
+}
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    WSADATA wsa;
+    SOCKET sock;
+    sockaddr_in servAddr, cliAddr;
+    int cliSize = sizeof(cliAddr);
+
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = INADDR_ANY;
+    servAddr.sin_port = htons(SRV_PORT);
+
+    bind(sock, (sockaddr*)&servAddr, sizeof(servAddr));
+
+    cout << "UDP сервер запущен..." << endl;
+
+    while (true) {
+        int received = recvfrom(sock, (char*)&S, sizeof(S), 0,
+            (sockaddr*)&cliAddr, &cliSize);
+
+        if (received > 0) {
+            if (string(S.name) == "exit") {
+                cout << "Клиент вышел" << endl;
+                continue;
+            }
+
+            char ip[INET_ADDRSTRLEN];
+            InetNtopA(AF_INET, &(cliAddr.sin_addr), ip, INET_ADDRSTRLEN);
+
+            cout << "От " << ip << ": " << S.name << endl;
+            cout << "Введите оценки за экзамены: ";
+            for (int i = 0; i < 4; i++) {
+                cout << S.grades[i]<<"  ";
+            }
+
+            string bonus = calcScholarship();
+            cout << "Стипендия: " << bonus << endl;
+
+            sendto(sock, bonus.c_str(), bonus.size() + 1, 0,
+                (sockaddr*)&cliAddr, cliSize);
+        }
+    }
+
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
