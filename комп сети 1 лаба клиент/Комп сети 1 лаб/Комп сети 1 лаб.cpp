73f034d581be485aade0c8577a7ff52edb90b19a
Diana Saakyan <diana.saakyan003@gmail.com>
2026-01-04 18:01:58 +0300

    Add files via upload

---
+++ комп сети 1 лаба клиент/Комп сети 1 лаб/Комп сети 1 лаб.cpp
@@ -0,0 +1,84 @@
+﻿//программа-сервер
+
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+struct Person {
+    char name[25];
+    int height;
+    int weight;
+};
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    // Используем абсолютные пути
+    string nameR = "C:\\server\\REQUEST.txt";
+    string nameA = "C:\\server\\ANSWER.txt";
+
+    Person B;
+    int answer;
+
+    cout << "Сервер работает и ждет запросов..." << endl;
+    cout << "файл запроса: " << nameR << endl;
+    cout << "файл ответа: " << nameA << endl;
+
+    while (true) {
+        // Пытаемся прочитать запрос
+        ifstream fR(nameR);
+        if (fR.is_open()) {
+            fR.read((char*)&B, sizeof(B));
+            if (fR.gcount() == sizeof(B)) {
+                cout << "**********НОВЫЙ ЗАПРОС***********" << endl;
+                cout << "Имя: " << B.name << endl;
+                cout << "Рост: " << B.height << "cм, Вес: " << B.weight << "кг" << endl;
+
+                // Вычисляем ИМТ
+                double height_m = B.height * 0.01;
+                double IMT = B.weight / (height_m * height_m);
+
+                cout << "Индекс массы тела: " << IMT << endl;
+
+                if (IMT < 18.5) {
+                    answer = 0;
+                    cout << "Результат: недобор веса" << endl;
+                }
+                else if (IMT < 25) {
+                    answer = 1;
+                    cout << "Результат: норма" << endl;
+                }
+                else {
+                    answer = 2;
+                    cout << "Результат: масса тела выше нормы " << endl;
+                }
+
+                // Записываем ответ
+                ofstream fA(nameA);
+                if (fA.is_open()) {
+                    fA.write((char*)&answer, sizeof(answer));
+                    fA.close();
+                    cout << "Данные успешно отправлены!" << endl;
+                }
+                else {
+                    cout << "Ошибка: не удалось создать файл ответа" << endl;
+                }
+
+                // Удаляем обработанный запрос
+                fR.close();
+                if (remove(nameR.c_str()) != 0) {
+                    cout << "Не удалось удалить запрос" << endl;
+                }
+
+                cout << "Следующий запрос?" << endl << endl;
+            }
+            else {
+                fR.close();
+            }
+        }
+        Sleep(1000); // Проверяем каждую секунду
+    }
+    return 0;
+}
---
+++ комп сети 1 лаба сервер/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,103 @@
+﻿//программа-клиент
+
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+struct Person {
+    char name[25];
+    int height;
+    int weight;
+};
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    string nameR = "C:\\server\\REQUEST.txt";
+    string nameA = "C:\\server\\ANSWER.txt";
+
+    Person B;
+    int answer;
+
+    cout << "********* КЛИЕНТ *********" << endl;
+
+    cout << "Введите имя: ";
+    cin >> B.name;
+    cout << "Введите рост(cм): ";
+    cin >> B.height;
+    cout << "Введите вес(кг): ";
+    cin >> B.weight;
+
+    // Удаляем старый файл ответа если существует
+    remove(nameA.c_str());
+
+    // Записываем запрос
+    ofstream fR(nameR);
+    if (!fR.is_open()) {
+        cout << "Ошибка создания ответа от файла" << endl;
+        cout << "Проверьте папку С:/server" << endl;
+
+        // Попробуем создать в текущей директории
+        cout << "Пробуем создать файл в нужной директории" << endl;
+        nameR = "REQUEST.bin";
+        nameA = "ANSWER.bin";
+
+        fR.open(nameR, ios::binary);
+        if (!fR.is_open()) {
+            cout << "Не удалось оздать файл" << endl;
+            cout << "Нажмите Enter для выхода";
+            cin.ignore();
+            cin.get();
+            return 1;
+        }
+    }
+
+    fR.write((char*)&B, sizeof(B));
+    fR.close();
+
+    cout << "Запрос отправлен, ожидание ответа..." << endl;
+
+    // Ждем ответ (максимум 10 секунд)
+    bool gotResponse = false;
+    for (int i = 0; i < 100; i++) {
+        ifstream fA(nameA);
+        if (fA.is_open()) {
+            fA.read((char*)&answer, sizeof(answer));
+            if (fA.gcount() == sizeof(answer)) {
+                gotResponse = true;
+                fA.close();
+                break;
+            }
+            fA.close();
+        }
+        cout << "."; // Прогресс
+        Sleep(100); // Ждем 100ms
+    }
+    cout << endl;
+
+    if (gotResponse) {
+        cout << "************** РЕЗУЛЬТАТ ***************" << endl;
+        cout << "Имя: " << B.name << endl;
+        cout << "Рост: " << B.height << "cм, Вес: " << B.weight << "кг" << endl;
+        cout << "Результат по введенным данным: ";
+        switch (answer) {
+        case 0: cout << "недобор веса"; break;
+        case 1: cout << "нормальный вес"; break;
+        case 2: cout << "избыточный вес"; break;
+        default: cout << "неизвестно"; break;
+        }
+        cout << endl;
+    }
+    else {
+        cout << "Ошибка: не ответа от сервера" << endl;
+        cout << "Проверьте работу сервера и нахождение файлов в указанной папке" << endl;
+    }
+
+    cout << "Нажмите Enter для выхода";
+    cin.ignore();
+    cin.get();
+
+    return 0;
+}
